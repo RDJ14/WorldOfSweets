8bad155a1231cf89a1fb4b69fcbf4c10ec5eba80
Malekagr <maa245@pitt.edu>
2017-10-30 14:11:47 -0400

    Add files via upload

---
+++ src/test/java/com/mcteamface/worldofsweets/GameBoardTest.java
@@ -0,0 +1,31 @@
+package com.mcteamface.worldofsweets;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameBoardTest {
+
+	@Test
+	public void test1() {
+		GameBoard g = new GameBoard(1);
+		assert(g.p == 1);
+	}
+	@Test
+	public void test2() {
+		GameBoard g = new GameBoard(2);
+		assert(g.p == 2);
+	}
+	@Test
+	public void test3() {
+		GameBoard g = new GameBoard(3);
+		assert(g.p == 3);
+	}
+	@Test
+	public void test4() {
+		GameBoard g = new GameBoard(4);
+		assert(g.p == 4);
+	}
+
+
+
+}
