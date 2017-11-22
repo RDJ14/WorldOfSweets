61bc0ed991da7fb4df1103a5310e55523b033120
Nick Bourdakos <bourdakos1@gmail.com>
2017-11-21 21:02:13 -0500

    deck functionality

---
+++ src/main/java/com/mcteamface/worldofsweets/Cards.java
@@ -0,0 +1,34 @@
+package com.mcteamface.worldofsweets;
+
+import javax.swing.ImageIcon;
+import java.net.URL;
+import java.awt.Image;
+
+public enum Cards {
+  RED("/images/card_single_red.png"),
+  YELLOW("/images/card_single_yellow.png"),
+  BLUE("/images/card_single_blue.png"),
+  GREEN("/images/card_single_green.png"),
+  ORANGE("/images/card_single_orange.png"),
+  DOUBLE_RED("/images/card_double_red.png"),
+  DOUBLE_YELLOW("/images/card_double_yellow.png"),
+  DOUBLE_BLUE("/images/card_double_blue.png"),
+  DOUBLE_GREEN("/images/card_double_green.png"),
+  DOUBLE_ORANGE("/images/card_double_orange.png"),
+  SPECIAL_LOLLIPOP("/images/card_special_lollipop.png"),
+  SPECIAL_CORDIAL("/images/card_special_cordial.png"),
+  SPECIAL_CANDY_CANE("/images/card_special_candycane.png"),
+  SPECIAL_GUM_DROP("/images/card_special_gumdrop.png"),
+  SPECIAL_NOUGAT("/images/card_special_nougat.png");
+
+  private final String stringLocation;
+
+  private Cards(final String stringLocation) {
+    this.stringLocation = stringLocation;
+  }
+
+  public Image getImage() {
+    URL url = getClass().getResource(stringLocation);
+    return new ImageIcon(url).getImage();
+  }
+}
--- src/main/java/com/mcteamface/worldofsweets/models/DeckModel.java
+++ src/main/java/com/mcteamface/worldofsweets/models/DeckModel.java
@@ -2,21 +2,56 @@ package com.mcteamface.worldofsweets;
 
 import javax.swing.ImageIcon;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class DeckModel {
-  GameBoardView mGameBoardView;
+  public static final int NUM_SINGLE = 10;
+  public static final int NUM_DOUBLE = 2;
+  public static final int NUM_SPECIAL = 1;
+
+  private GameBoardView mGameBoardView;
+  private ArrayList<Cards> mCards = new ArrayList<Cards>();
 
   public DeckModel(GameBoardView gameBoardView) {
     mGameBoardView = gameBoardView;
+    initDeck();
     initListeners();
+  }
+
+  private void initDeck() {
+    for (int i = 0; i < NUM_SINGLE; i++) {
+      mCards.add(Cards.RED);
+      mCards.add(Cards.YELLOW);
+      mCards.add(Cards.BLUE);
+      mCards.add(Cards.GREEN);
+      mCards.add(Cards.ORANGE);
+    }
+
+    for (int i = 0; i < NUM_DOUBLE; i++) {
+      mCards.add(Cards.DOUBLE_RED);
+      mCards.add(Cards.DOUBLE_YELLOW);
+      mCards.add(Cards.DOUBLE_BLUE);
+      mCards.add(Cards.DOUBLE_GREEN);
+      mCards.add(Cards.DOUBLE_ORANGE);
+    }
+
+    for (int i = 0; i < NUM_SPECIAL; i++) {
+      mCards.add(Cards.SPECIAL_LOLLIPOP);
+      mCards.add(Cards.SPECIAL_CORDIAL);
+      mCards.add(Cards.SPECIAL_CANDY_CANE);
+      mCards.add(Cards.SPECIAL_GUM_DROP);
+      mCards.add(Cards.SPECIAL_NOUGAT);
+    }
+
+    Collections.shuffle(mCards);
   }
 
   private void initListeners() {
     mGameBoardView.addCardDrawnListener(new GameBoardView.CardDrawnListener() {
       @Override
       public void cardDrawn() {
-        URL urlDiscardImg = getClass().getResource("/images/card_special_lollipop.png");
-        mGameBoardView.setDiscard(new ImageIcon(urlDiscardImg).getImage());
+        mGameBoardView.setDiscard(mCards.remove(0).getImage());
         mGameBoardView.repaint();
       }
     });
